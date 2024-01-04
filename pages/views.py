f77a52665e9e5fb0a7fc1caf4b9361a6f48b67f5
Creiwry <jay.gardos@outlook.com>
2024-01-04 18:55:06 +0100

    feat(backend): add signup and signin

--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from products.models import Product
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
 
 # Create your views here.
 def home_view(request, *args, **kwargs):
@@ -17,3 +20,31 @@ def about_view(request, *args, **kwargs):
 
 def contact_view(request, *args, **kwargs):
     return render(request, "contact.html", {})
+
+def signup_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'users/signup.html', {'form': form})
+
+def signin_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'users/signin.html', {'form': form})
--- trydjango/urls.py
+++ trydjango/urls.py
@@ -15,12 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth.views import LoginView
 from django.urls import path, include
-from pages.views import home_view, contact_view, about_view
+from pages.views import home_view, contact_view, about_view, signup_view, signin_view
 from products.views import product_detail_view, product_delete_view, product_create_view, product_update_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('signup/', signup_view),
+    path('signin/', LoginView.as_view(template_name='users/signin.html'), name='login'),
     path('', home_view, name='home'),
     path('product/<int:product_id>', product_detail_view, name="product_detail"),
     path('product/<int:product_id>/update', product_update_view, name="product_update"),
